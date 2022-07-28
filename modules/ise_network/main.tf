e1afdbf693610a4ed45ec57c3fc2f6878d0a0b85
fmunoz <fmunoz@altus.cr>
2022-07-28 16:18:40 -0600

    new rule

--- modules/ise_network/main.tf
+++ modules/ise_network/main.tf
@@ -48,8 +48,20 @@ resource "aws_security_group" "ise_security_group" {
   }
   ingress {
     protocol    = "icmp"
-    from_port   = 1
-    to_port     = 1
+    from_port   = -1
+    to_port     = -1
     cidr_blocks = [var.aws_public_access_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
